5ecf2f831dadb5767898358705f43af975d4abca
mtorials <git@mtorials.de>
2021-12-18 11:38:36 +0100

    conf

--- src/defs.hpp
+++ src/defs.hpp
@@ -1,9 +1,9 @@
 #pragma once
 #include "Colors.hpp"
 
-#define TOUCH_THRESHOLD 100
+#define TOUCH_THRESHOLD 60
 #define GRAPH_TIME_LINE_THICKNESS 2
-#define OUTPUT_PIN 47
+#define OUTPUT_PIN 31
 #define TEMP_THRESHOLD 10
 
 #define LABEL_SIZE 0.9
